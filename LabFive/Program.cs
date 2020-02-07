e3b7fe889dd4edc0e234a27e632b6e493987d299
Vlad Zelinskyi <vladyslav.zelinkyi@uklon.com.ua>
2020-02-07 18:07:19 +0200

    lab5

--- LabFive/Program.cs
+++ LabFive/Program.cs
@@ -6,18 +6,78 @@ namespace LabFive
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var matrix1 = Randomizer.GetMatrixWithRandomNumbers(3, 5);
+            var matrix2 = Randomizer.GetMatrixWithRandomNumbers(5, 3);
+
+            Console.WriteLine("First matrix:");
+            Console.WriteLine("Input:");
+            Printer(matrix1);
+
+            Console.WriteLine("Output:");
+            ClearNe(matrix1);
+            Printer(matrix1);
+
+            Console.WriteLine("Second matrix:");
+            Console.WriteLine("Input:");
+            Printer(matrix2);
+
+            Console.WriteLine("Output:");
+            ClearNe(matrix2);
+            Printer(matrix2);
         }
 
-        void ClearNe(int[,] matrix)
+        static void ClearNe(int[][] matrix)
         {
-            for (int i = 0; i < matrix.GetLength(1); i++)
+            for (int i = 0; i < matrix.Length; i++)
             {
-                for (int j = 0; j < matrix.GetLength(2); j++)
+                for (int j = 0; j < matrix[i].Length; j++)
                 {
-                    
+                    if (i <= j)
+                    {
+                        matrix[i][j] = 0;
+                    }
                 }
             }
+        }
+
+        static void Printer(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.Write($"( ");
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write($"{matrix[i][j]} ");
+                }
+
+                Console.WriteLine(")\n");
+            }
+        }
+    }
+
+    public class Randomizer
+    {
+        public static int[][] GetMatrixWithRandomNumbers(int lines, int columns)
+        {
+            if (lines < 2 || columns < 2)
+            {
+                throw new ArgumentException("Parameters should be greater than 2.");
+            }
+
+            var random = new Random();
+
+            var result = new int[lines][];
+            
+            for (int i = 0; i < lines; i++)
+            {
+                result[i] = new int[columns];
+                for (int j = 0; j < columns; j++)
+                {
+                    result[i][j] = random.Next(-10, 10);
+                }
+            }
+
+            return result;
         }
     }
 }
